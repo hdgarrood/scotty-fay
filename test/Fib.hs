f683c0ebab9b22c447bf4a162eada5fb50949e58
Harry Garrood <hdgarrood@gmail.com>
2013-12-09 16:45:46 +0000

    Add (failing) tests

---
+++ test/Fib.hs
@@ -0,0 +1,8 @@
+module Fib where
+
+-- A near-trivial Fay module for testing scotty-fay
+
+import Prelude
+
+fact :: Int -> Int
+fact n = product [1..n]
---
+++ test/TestMain.hs
@@ -0,0 +1,62 @@
+module Main where
+
+import Control.Monad.IO.Class (liftIO)
+import Web.Scotty hiding (request)
+import Web.Scotty.Fay
+import qualified Network.HTTP.Types as HTTP
+import Network.Wai
+import Network.Wai.Test
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import qualified Test.HUnit.Base as H
+import System.Directory
+
+main :: IO ()
+main = do
+    dir <- getCurrentDirectory
+    putStrLn dir
+    tests >>= defaultMain
+
+tests :: IO [Test]
+tests = sequence
+    [ waiTest "compiles Fay" test_compilesFay
+    , waiTest "serveFay captures everything under base" test_capturesEverything
+    ]
+
+waiTest :: String -> Session () -> IO Test
+waiTest name session = do
+    app' <- scottyApp app
+    return $ testCase name $ runSession session app'
+
+app :: ScottyM ()
+app = do
+    serveFay "/fay"
+
+    get "/fay/shouldnt-get-here" $ do
+        text "it shouldn't get here"
+
+assertBool :: String -> Bool -> Session ()
+assertBool str p = liftIO $ H.assertBool str p
+
+assertNotStatus :: Int -> SResponse -> Session ()
+assertNotStatus i SResponse{simpleStatus = s} = assertBool (concat
+    [ "Expected a status other than "
+    , show i
+    ]) $ i /= sc
+    where
+        sc = HTTP.statusCode s
+
+test_compilesFay :: Session ()
+test_compilesFay = do
+    let req = setPath defaultRequest "/fay/test/Fib.hs"
+    resp <- request req
+
+    assertStatus 200 resp
+    assertHeader "Content-Type" "text/javascript" resp
+
+test_capturesEverything :: Session ()
+test_capturesEverything = do
+    let req = setPath defaultRequest "/fay/shouldnt-get-here"
+    resp <- request req
+
+    assertNotStatus 200 resp
