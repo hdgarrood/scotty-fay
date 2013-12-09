00add1895b9cb3c8850e1cd77ae3c3f4f9e3380f
Harry Garrood <hdgarrood@gmail.com>
2013-12-09 15:28:18 +0000

    Implement proper path matching
    * Allow paths with multiple segments after base, eg /fay/foo/bar.js
    * Prevent directory traversal

--- src/Web/Scotty/Fay.hs
+++ src/Web/Scotty/Fay.hs
@@ -1,18 +1,21 @@
 module Web.Scotty.Fay (serveFay) where
 
+import Control.Monad (guard)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Default
-import Data.Text.Lazy (Text)
+import Data.Maybe (listToMaybe)
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text as T
+import Network.Wai (pathInfo)
 import Web.Scotty.Trans
 import Fay
 
 import Web.Scotty.Fay.Utils
 
-serveFay :: MonadIO a => String -> ScottyT Text a ()
+serveFay :: MonadIO a => T.Text -> ScottyT LT.Text a ()
 serveFay base = do
-    get (capture $ base ++ "/:path") $ do
-        -- TODO: security: directory traversal
-        path <- param "path"
+    get (makeFayPattern base) $ do
+        path <- param "filePath"
         result <- liftIO (compileFile config path)
         case result of
             Left err        -> raiseCompileErr err
@@ -20,10 +23,34 @@ serveFay base = do
     where
         raiseCompileErr = raise . stringToLazyText . showCompileError
 
+makeFayPattern :: T.Text -> RoutePattern
+makeFayPattern base = function $ \req -> do
+    let pathSegments = pathInfo req
+    first <- safeHead pathSegments
+    rest  <- nonEmptyTail pathSegments
+
+    guard (first == base)
+    filePath <- secureRejoin rest
+
+    return [("filePath", LT.fromStrict filePath)]
+    where
+        safeHead            = listToMaybe
+
+        nonEmptyTail []     = Nothing
+        nonEmptyTail [x]    = Nothing
+        nonEmptyTail (x:xs) = Just xs
+
+secureRejoin :: [T.Text] -> Maybe T.Text
+secureRejoin xs = do
+    guard (noDots xs)
+    return $ T.intercalate "/" xs
+    where
+        noDots = not . elem ".."
+
 config :: CompileConfig
 config = def
 
-js :: MonadIO a => String -> ActionT Text a ()
+js :: MonadIO a => String -> ActionT LT.Text a ()
 js jsString = do
     setHeader "Content-Type" "text/javascript"
     raw $ stringToLazyByteString jsString
