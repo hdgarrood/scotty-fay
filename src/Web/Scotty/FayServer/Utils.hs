028690e117ff52fee1b65b56849e270d25145543
Harry Garrood <hdgarrood@gmail.com>
2013-12-09 00:10:37 +0000

    forgot to add this in previous commit

---
+++ src/Web/Scotty/FayServer/Utils.hs
@@ -0,0 +1,17 @@
+module Web.Scotty.FayServer.Utils where
+
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.ByteString.Lazy.Char8 as LBC8
+import qualified Data.ByteString as B
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy.Encoding as LTE
+import qualified Data.Text.Encoding as TE
+
+stringToLazyByteString :: String -> LB.ByteString
+stringToLazyByteString = LBC8.pack
+
+stringToLazyText :: String -> LT.Text
+stringToLazyText = LTE.decodeUtf8 . stringToLazyByteString
+
+strictByteStringToLazyText :: B.ByteString -> LT.Text
+strictByteStringToLazyText = LT.fromStrict . TE.decodeUtf8
